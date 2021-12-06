3e489e992b1f1bc7a5d26368969e45898417cdf0
Sahil Sharma <40673232+sahil315@users.noreply.github.com>
2021-12-06 11:18:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,9 @@ const dotenv = require("dotenv")
 dotenv.config() 
 
 
-// const PORT = process.env.PORT
+const PORT = process.env.PORT
 
 const app = express()
-app.use(helmet())
-
-app.use(require("./routes/index.js"))
-
-
-app.listen(process.env.PORT, function () {
-  console.log(`Express app listening on port ${process.env.PORT}`)
-
-})
-app.get('/', (req, res) => {
-  res.send("hello world")
-})
-
 
 const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uooen.mongodb.net/My_Blog?retryWrites=true&w=majority`
 
@@ -38,20 +25,12 @@ mongoose.connection.on("open", function() {
   console.log("Connected to MongoDB database.")
 })
 
-// mongoose.connect(config.database, (error) => {
-//   if(error) console.error(error);
-//   else console.log('connected to db ' + config.database);
-// });
+app.use(helmet())
+
+app.use(require("./routes/index.js"))
 
 
-// app.use(helmet())
+app.listen(process.env.PORT, function () {
+  console.log(`Express app listening on port ${PORT}`)
 
-// app.use(require("./routes/index.js"))
-// app.listen(PORT, function () {
-//   console.log(`Express app listening on port ${PORT}`)
-// })
-// app.get("/", function(req, res) {
-//   res.send("hello world")
-// })
-
-
+})
