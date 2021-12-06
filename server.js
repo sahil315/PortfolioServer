e6561a847eded1e635da46a843a0bb014d2d9e99
Sahil Sharma <40673232+sahil315@users.noreply.github.com>
2021-12-06 13:36:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ mongoose.connection.on("open", function() {
 app.use(helmet())
 
 app.use(require("./routes/index.js"))
-
+app.get(req,res) => {
+    res.send("hello");
+}
 
 app.listen(process.env.PORT, function () {
   console.log(`Express app listening on port ${PORT}`)
